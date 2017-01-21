0e1a396fb8793117fe791b301492e679207a8b56
pat310 <patrick.trasborg@gmail.com>
2017-01-21 11:55:10 -0500

    adding tests for fixDataFormat private function

--- logic.js
+++ logic.js
@@ -1,16 +1,5 @@
 'use strict';
 
-var data = [
-  {name: 'patrick', borough: 'brooklyn', age: '28', gender: 'm'},
-  {name: 'greg', borough: 'brooklyn', age: '29', gender: 'm'},
-  {name: 'niles', borough: 'manhattan', age: '30', gender: 'm'},
-  {name: 'jared', borough: 'manhattan', age: '29', gender: 'm'},
-  {name: 'markus', borough: 'manhattan', age: '28', gender: 'm'},
-  {name: 'sarah', borough: 'queens', age: '30', gender: 'f'},
-  {name: 'vishakh', borough: 'queens', age: '28', gender: 'm'},
-  {name: 'jessica', borough: 'brooklyn', age: '28', gender: 'f'}
-];
-
 function fixDataFormat(data){
  if(!Array.isArray(data) || !data.length) return [];
  else if(typeof data[0] === 'object' && !Array.isArray(data[0])) return data;
@@ -30,26 +19,6 @@ function fixDataFormat(data){
   },[]);
  }
 }
-
-//console.log(fixDataFormat(data));
-//var data2 = ['name', 'patrick', 'bill', 'greg'];
-//console.log(fixDataFormat(data2));
-// var data3 = [
-//   ['name', 'borough', 'age', 'gender'],
-//   ['patrick', 'brooklyn', '28', 'm'],
-//   ['greg', 'brooklyn', '29', 'm'],
-//   ['niles', 'manhattan', '30', 'm'],
-//   ['jared', 'manhattan', '29', 'm'],
-//   ['markus', 'manhattan', '28', 'm'],
-//   ['sarah', 'queens', '30', 'f'],
-//   ['vishakh', 'queens', '28', 'm'],
-//   ['jessica', 'brooklyn', '28', 'f']
-// ];
-//console.log(fixDataFormat(data3));
-//var data4='hello';
-//console.log(fixDataFormat(data4));
-//var data5 = [];
-//console.log(fixDataFormat(data5));
 
 function groupByCategory(data, groupBy){
   return data.reduce((acc, curr) =>{
--- test.js
+++ test.js
@@ -9,12 +9,67 @@ const {
   tableCreator
 } = require('./logic');
 
-console.log('this is fixDataFormat', fixDataFormat);
+const chai = require('chai');
+const expect = chai.expect;
 
-var chai = require('chai').expect;
-const assert = require('assert');
-describe('Array', function(){
-  it('should return -1', function(){
-    assert.equal(-1, [1,2,3].indexOf(4));
+describe('fixDataFormat', function(){
+  function isEqualCheck(data1, data2){
+    if(data1.length !== data2.length) return false;
+    return data1.every((dataRow, i) =>{
+      return Object.keys(dataRow).every(rowKey =>{
+        return dataRow[rowKey] === data2[i][rowKey];
+      });
+    });
+  }
+
+  var data = [
+    {name: 'patrick', borough: 'brooklyn', age: '28', gender: 'm'},
+    {name: 'greg', borough: 'brooklyn', age: '29', gender: 'm'},
+    {name: 'niles', borough: 'manhattan', age: '30', gender: 'm'},
+    {name: 'jared', borough: 'manhattan', age: '29', gender: 'm'},
+    {name: 'markus', borough: 'manhattan', age: '28', gender: 'm'},
+    {name: 'sarah', borough: 'queens', age: '30', gender: 'f'},
+    {name: 'vishakh', borough: 'queens', age: '28', gender: 'm'},
+    {name: 'jessica', borough: 'brooklyn', age: '28', gender: 'f'}
+  ];
+
+  it('should leave data alone if it is properly formatted', function(){
+    var newData = fixDataFormat(data);
+    expect(isEqualCheck(data, newData)).to.be.true;
+  });
+
+  it('should turn an array of data into array of objects', function(){
+    var arrayData = [
+      ['name', 'borough', 'age', 'gender'],
+      ['patrick', 'brooklyn', '28', 'm'],
+      ['greg', 'brooklyn', '29', 'm'],
+      ['niles', 'manhattan', '30', 'm'],
+      ['jared', 'manhattan', '29', 'm'],
+      ['markus', 'manhattan', '28', 'm'],
+      ['sarah', 'queens', '30', 'f'],
+      ['vishakh', 'queens', '28', 'm'],
+      ['jessica', 'brooklyn', '28', 'f']
+    ];
+
+    var newData = fixDataFormat(arrayData);
+    expect(isEqualCheck(data, newData)).to.be.true;
+  });
+
+  it('should turn an array of data into an array of objects', function(){
+    var data = ['name', 'patrick', 'bill', 'greg'];
+    var newData = fixDataFormat(data);
+    var expectedData = [{name: 'patrick'}, {name: 'bill'}, {name: 'greg'}];
+    expect(isEqualCheck(expectedData, newData)).to.be.true;
+  });
+
+  it('should return an empty array if given an empty array', function(){
+    var newData = fixDataFormat([]);
+    expect(newData).to.be.empty;
+  });
+
+  it('should return an empty array if given something other than an array', function(){
+    var newData = fixDataFormat('i am data');
+    expect(Array.isArray(newData)).to.be.true;
+    expect(newData).to.be.empty;
   });
 });
