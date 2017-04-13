812bc6ab5f6e5dfb205dc791877cd7359e85b10f
pat310 <patrick.trasborg@gmail.com>
2017-04-13 18:32:54 -0400

    adding comments about error

--- src/logic.js
+++ src/logic.js
@@ -200,7 +200,10 @@ export function checkPivotCategories(actualCats, selectedCats) {
 export function tableCreator(data, rows = [], cols = [], accCatOrCB,
   accTypeOrInitVal, rowHeader) {
 
+  /** if data is empty, return empty array */
   if (data.length === 0) return [];
+
+  /** if rows/cols are not arrays, return throw an error */
   if (!Array.isArray(rows) || !Array.isArray(cols)) {
     throw new Error('rowsToPivot and colsToPivot must be of type array');
   }
